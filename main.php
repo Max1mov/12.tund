a8654da32515a550386a1fb8c7442fdc65421fa4
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-11-27 11:34:53 +0200

    lalala

---
+++ main.php
@@ -0,0 +1,64 @@
+<?php require_once("header.php"); ?>
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<br><br><br>
+<!-- ¤¤¤¤¤¤¤SISU¤¤¤¤¤¤¤¤ -->
+<!-- ¤¤¤¤¤¤¤SISU¤¤¤¤¤¤¤¤ -->
+<!-- ¤¤¤¤¤¤¤SISU¤¤¤¤¤¤¤¤ -->
+<!-- ¤¤¤¤¤¤¤SISU¤¤¤¤¤¤¤¤ -->
+
+<div class="container-fluid">
+
+
+	<div class="row">
+	
+		<div class="col-md-6 col-offset-md-1 col-sm-8">vasak
+			<div class="jumbotron">
+				<h1>Hello, world!</h1>
+				<p>...</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+			</div>
+		</div>
+		<div class="col-md-2 col-offset-md-1 col-sm-4">
+			LOGIN
+			<form>
+				<div class="form-group">
+					<label for="exampleInputEmail1"></label>
+					<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+				</div>
+				<div class="form-group">
+					<label for="exampleInputPassword1"></label>
+					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				</div>
+				
+				<button type="submit" class="btn btn-success pull-right visible-xs-">Submit</button>
+				<button type="button" class="btn btn-primary btn-lg btn-block visible-sm-">Block level button</button>
+			</form>
+	</div>
+	</div>
+
+
+<?php require_once("footer.php"); ?>
